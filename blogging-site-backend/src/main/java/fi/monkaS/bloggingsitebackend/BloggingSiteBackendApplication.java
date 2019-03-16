cfdb12cac3e17254a077ccf366c4e7929f622227
Toni Maijala <tonimaijalasd@gmail.com>
2019-03-16 12:29:41 +0200

    Initializing project

---
+++ blogging-site-backend/src/main/java/fi/monkaS/bloggingsitebackend/BloggingSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package fi.monkaS.bloggingsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingSiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingSiteBackendApplication.class, args);
+	}
+
+}
---
+++ blogging-site-backend/src/test/java/fi/monkaS/bloggingsitebackend/BloggingSiteBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.monkaS.bloggingsitebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BloggingSiteBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
