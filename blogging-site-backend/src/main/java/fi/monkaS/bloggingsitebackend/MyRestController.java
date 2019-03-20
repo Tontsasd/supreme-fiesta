a7ef5033585773e532cc3770f383a0f2922f139a
Toni Maijala <tonimaijalasd@gmail.com>
2019-03-20 10:34:16 +0200

    Commented out a method for release purposes.

--- blogging-site-backend/src/main/java/fi/monkaS/bloggingsitebackend/MyRestController.java
+++ blogging-site-backend/src/main/java/fi/monkaS/bloggingsitebackend/MyRestController.java
@@ -27,7 +27,7 @@ public class MyRestController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-    @RequestMapping
+    /*@RequestMapping
     public ResponseEntity<Void> updatePost(@RequestBody Blogpost p, UriComponentsBuilder b) {
         blogpostRepository.update(p);
 
@@ -36,7 +36,7 @@ public class MyRestController {
         headers.setLocation(uriComponents.toUri());
 
         return new ResponseEntity<>(headers, HttpStatus.OK);
-    }
+    }*/
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public Iterable<Blogpost> getAllPosts() {
